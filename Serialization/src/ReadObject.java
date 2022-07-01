52d27bd84dfe2f54294982034a8a9443dff8c5db
Glynda Humberto <ghumbert@un-m6.kzn.21-school.ru>
2022-07-01 16:19:38 +0300

    сериализация

--- Serialization/src/ReadObject.java
+++ Serialization/src/ReadObject.java
@@ -1,26 +1,21 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class ReadObject {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        try {
             FileInputStream fis = new FileInputStream("people.bin");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
-            int personCount = ois.readInt(); // exception
-            Person[] people = new Person[personCount];
-
-            for (int i = 0; i < personCount; i++){
-                people[i] = (Person) ois.readObject();
-            }
-
+            int personCount = ois.readInt();
+			Person[] people = new Person[personCount];
+			for (int i = 0; i < personCount; i++) {
+				people[i] = (Person) ois.readObject();
+			}
+            System.out.println(Arrays.toString(people));
             ois.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 }
--- Serialization/src/WriteObject.java
+++ Serialization/src/WriteObject.java
@@ -1,15 +1,20 @@
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 
 public class WriteObject {
     public static void main(String[] args) {
-        Person[] people = {new Person(1, "Biba"), new Person(2, "Boba"), new Person(3, "Mike")};
-
+        Person[] people = {
+                new Person(1, "Biba"),
+                new Person(2, "Boba"),
+                new Person(3, "Mike"),
+        };
         try {
             FileOutputStream fos = new FileOutputStream("people.bin");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-            oos.writeObject(people.length);
-
+            oos.writeInt(people.length);
             for(Person person : people){
                 oos.writeObject(person);
             }
